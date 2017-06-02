e1bf35cc2ce1e0703c9cbc4441157d975df357a9
TheoLefebvre98 <lefebvre.theo98@gmail.com>
2017-06-02 15:24:22 +0700

    db change

--- server/database.py
+++ server/database.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
 
-import mysql.connector
+import MySQLdb
 
 class Database:
 
 	def __init__(self, plocalhost, puser, ppassword, pdatabase):
 
-		self.conn = mysql.connector.connect(host=plocalhost,user=puser,password=ppassword, database=pdatabase)
+		self.conn = MySQLdb.connect(host=plocalhost,user=puser,passwd=ppassword, db=pdatabase)
 		self.cursor = self.conn.cursor()
 
 		self.cursor.execute("""
@@ -16,14 +16,15 @@ class Database:
 			""")
 	
 	def loginUser(self, username, password):
-
-
-	def registerUser(self, username, password, email):
-
-	def storeMsg(self, sender, receiver, content):
-
-	def findLastMsg(self, sender, receiver):
+		self.cursor.execute("""SELECT id FROM users WHERE username=? AND password=? """, (username,password,))
+		rows = self.cursor.fetchall()
+		for row in rows:
+			return True
+		return False
 
 	def dispose(self):
 		self.conn.close()
 
+
+db = Database("localhost","root","root","chatAppDB")
+print db.loginUser("pierre","mdp")
