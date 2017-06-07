967d6697753c8f4274805376fb77e5db0babceaa
TheoLefebvre98 <lefebvre.theo98@gmail.com>
2017-06-07 10:37:56 +0700

    change database

--- server/database.py
+++ server/database.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
 import pymysql
@@ -13,12 +13,17 @@ class Database:
                              db=db,
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-		cur = self.conn.cursor()
+		cursor = self.conn.cursor()
 
-		cur.execute("""
+		cursor.execute("""
 			CREATE TABLE IF NOT EXISTS users (id int(5) NOT NULL AUTO_INCREMENT, username varchar(50) DEFAULT NULL, 
 			password varchar(50) DEFAULT NULL, email varchar(100) DEFAULT NULL, PRIMARY KEY(id) );
 			""")
+		cursor.execute("""
+			CREATE TABLE IF NOT EXISTS message (id int(5) NOT NULL AUTO_INCREMENT, sender varchar(50) DEFAULT NULL, 
+			receiver varchar(50) DEFAULT NULL, content varchar(1000) DEFAULT NULL, PRIMARY KEY(id) );
+			""")
+
 	
 	def loginUser(self, username, password):
 		find = False
@@ -51,6 +56,41 @@ class Database:
 		finally:
 			self.dispose()
 			return register
+
+	def test(args, types):
+    for a, t in zip(args, types):
+        if not isinstance(a, t):
+            return False
+        else:
+        	return True
+
+	def saveMsg(self, sender, receiver, content):		
+		if saved != test((sender, receiver, content), (str, str, str)):
+			raise TypeError("Mauvais type")
+		try:
+			with self.conn.cursor() as cursor:				
+				sql = "INSERT INTO `message` (`sender`, `receiver`, `content`) VALUES (%s, %s, %s)"
+				cursor.execute(sql, (sender, receiver, content))
+				self.conn.commit()
+		finally:
+			self.dispose()
+			return saved
+
+	def findMsg(self, sender, receiver):
+		find = False
+		try:
+			with self.conn.cursor() as cursor:
+				sql = "SELECT `content` FROM `message` WHERE `sender`=%s AND `receiver`=%s ORDER BY `id` DESC LIMIT 1"
+				cursor.execute(sql, (sender, receiver))
+				result = cursor.fetchone()
+				if result:
+					find = True
+				else:
+					print "No available messages"
+					find = False
+		finally:
+			self,dispose()
+			return find
 
 	def dispose(self):
 		self.conn.close()
